933003421f9c3398e31835e3fe838e87fa42f408
Mário Prada <mariosilvaprada@gmail.com>
2019-11-15 22:20:08 +0000

    updating

--- routes/api.js
+++ routes/api.js
@@ -3,7 +3,23 @@ const turbo = require('turbo360')({site_id: process.env.TURBO_APP_ID})
 const vertex = require('vertex360')({site_id: process.env.TURBO_APP_ID})
 const router = vertex.router()
 
+const Profile = require('../models/Profile');
 
+router.get('/profile', (req, res) => {
+    Profile.find()
+        .then(profiles => {
+            res.json({
+                confirmation: 'success',
+                data: profiles
+            })
+        })
+        .catch(err => {
+            res.json({
+                confirmation: 'fail',
+                message: err.message
+            })
+        })
+})
 
 
 
